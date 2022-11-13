ff89633ea55f8b5dd153f705d6e6d9371596ad4c
f01yy <79443616+f01yy@users.noreply.github.com>
2022-11-13 16:58:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('.burger');
+const navList = document.querySelector('.nav__list');
+menu.addEventListener('click', (event) => {
+  if (navList.style.display === 'flex') {
+    navList.style.display = 'none';
+    menu.style.transform = 'rotate(90deg)';
+  } else {
+    navList.style.display = 'flex';
+    menu.style.transform = 'rotate(-90deg)';
+  }
+});
