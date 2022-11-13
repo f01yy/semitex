4c9dc9af7b78b0de28d1ef43ad631bcca2ab1323
f01yy <79443616+f01yy@users.noreply.github.com>
2022-11-13 17:11:51 +0300

    fixed burger

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const navList = document.querySelector('.nav__list');
 menu.addEventListener('click', (event) => {
   if (navList.style.display === 'flex') {
     navList.style.display = 'none';
+    menu.style.position = 'absolute';
     menu.style.transform = 'rotate(90deg)';
   } else {
     navList.style.display = 'flex';
     menu.style.transform = 'rotate(-90deg)';
+    menu.style.position = 'fixed';
   }
 });
